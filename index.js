ae236bdf1ea1898dfd06b2ccc9f64c72a734422f
dmadust@gmail.com <dmadust@gmail.com>
2018-04-21 02:56:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments) {
-  playArray = [];
+  var playArray = [];
   for (var i = 0; i < musicians.length; i++) {
     playArray.push("${musicians[i]} plays ${instruments[i]}");
   }
