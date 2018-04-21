62a8925fbbbaef646e2a9b6820ed9ccfcd0827d8
dmadust@gmail.com <dmadust@gmail.com>
2018-04-21 03:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var i = 0;
   while (i < facts.length) {
-    
+    facts[i] += "!!!";
   }
+  return facts;
 }
